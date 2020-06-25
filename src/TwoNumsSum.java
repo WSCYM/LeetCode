cbd2fe71950b36700172dde57e1326d82539ab10
ym <2124867925@qq.com>
2020-06-25 21:07:51 +0800

    两数之和

--- src/TwoNumsSum.java
+++ src/TwoNumsSum.java
@@ -1,14 +1,23 @@
+//给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+//
+// 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
+//
+//
+//
+// 示例:
+//
+// 给定 nums = [2, 7, 11, 15], target = 9
+//
+//因为 nums[0] + nums[1] = 2 + 7 = 9
+//所以返回 [0, 1]
+//
+// Related Topics 数组 哈希表
+
+
 import java.util.HashMap;
 
-/**
- * @author ym
- * @version 1.0.0
- * @ClassName Easy
- * @Description TODO
- * @createTime 2020-06-25 21:01:00
- */
-public class TwoNumsSum {
-
+//leetcode submit region begin(Prohibit modification and deletion)
+class TwoNumsSum {
     public int[] twoSum(int[] nums, int target) {
         HashMap hashMap = new HashMap();
         for (int i = 0;i<nums.length;i++){
@@ -21,3 +30,4 @@ public class TwoNumsSum {
         return null;
     }
 }
+//leetcode submit region end(Prohibit modification and deletion)
