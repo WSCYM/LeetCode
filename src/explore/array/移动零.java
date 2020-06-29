37f5f4638bf78bcf842209b7520a370be135fd74
ym <2124867925@qq.com>
2020-06-29 10:50:36 +0800

    移动零

--- src/explore/array/移动零.java
+++ src/explore/array/移动零.java
@@ -4,8 +4,31 @@ package explore.array;
  * @author ym
  * @version 1.0.0
  * @ClassName 移动零
- * @Description TODO
+ * @Description
+ *
+ * 给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。
+ *
+ * 示例:
+ *
+ * 输入: [0,1,0,3,12]
+ * 输出: [1,3,12,0,0]
+ * 说明:
+ *
+ * 必须在原数组上操作，不能拷贝额外的数组。
+ * 尽量减少操作次数。
+ *
  * @createTime 2020-06-29 10:31:00
  */
 public class 移动零 {
+    public void moveZeroes(int[] nums) {
+        int j = 0;
+        for (int i = 0 ; i < nums.length ; i++) {
+            if (nums[i] != 0) {
+                nums[j++] = nums[i];
+            }
+        }
+        while (j<nums.length){
+            nums[j++] = 0;
+        }
+    }
 }
