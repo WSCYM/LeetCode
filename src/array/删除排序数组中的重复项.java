3d6c5634f566278bfa91014c1a2a9eb9a9b06844
ym <2124867925@qq.com>
2020-11-16 20:27:01 +0800

    1116

--- src/array/删除排序数组中的重复项.java
+++ src/array/删除排序数组中的重复项.java
@@ -1,4 +1,18 @@
 package array;
 
 public class 删除排序数组中的重复项 {
+    public int removeDuplicates(int[] nums) {
+        if (nums.length==0){
+            return 0;
+        }
+        int p = 0,v = Integer.MIN_VALUE;
+        for (int i=0;i<nums.length;i++){
+            if (nums[i]!=v){
+                nums[p] = nums[i];
+                v=nums[p];
+                p++;
+            }
+        }
+        return p;
+    }
 }
