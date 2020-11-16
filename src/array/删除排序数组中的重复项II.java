4f6bd42fdc3291baa2eec215b0668345e070d092
ym <2124867925@qq.com>
2020-11-16 20:27:40 +0800

    1116

--- src/array/删除排序数组中的重复项II.java
+++ src/array/删除排序数组中的重复项II.java
@@ -1,4 +1,21 @@
 package array;
 
 public class 删除排序数组中的重复项II {
+    public int removeDuplicates(int[] nums) {
+        int slow=0,count=1;
+        for (int i = 0;i<nums.length;i++){
+            if (slow!=i){
+                if (nums[slow]!=nums[i]){
+                    nums[++slow] = nums[i];
+                    count = 1;
+                } else if (nums[slow]==nums[i]){
+                    if (count<2){
+                        nums[++slow] = nums[i];
+                        count ++;
+                    }
+                }
+            }
+        }
+        return slow+1;
+    }
 }
--- src/array/移除元素.java
+++ src/array/移除元素.java
@@ -1,4 +1,21 @@
 package array;
 
 public class 移除元素 {
+    public int removeElement(int[] nums, int val) {
+        int n = nums.length;
+        int i = 0;
+        while (i<n) {
+            if (nums[i] == val) {
+                nums[i] = nums[n-1];
+                n--;
+            } else {
+                i++;
+            }
+        }
+        return n;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new 移除元素().removeElement(new int[]{3,2,2,3},3));
+    }
 }
