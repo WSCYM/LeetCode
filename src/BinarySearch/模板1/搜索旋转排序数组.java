f6b5cc27c2e804f3953b34983f1e7247cd7da9c1
ym <2124867925@qq.com>
2020-11-18 14:11:54 +0800

    1118

--- src/BinarySearch/模板1/搜索旋转排序数组.java
+++ src/BinarySearch/模板1/搜索旋转排序数组.java
@@ -1,22 +1,43 @@
-package BinarySearch.模板1;
+package BinarySearch;
 
-import java.util.Arrays;
-
+/**
+ * @author ym
+ * @version 1.0.0
+ * @ClassName 搜索旋转排序数组
+ * @Description TODO
+ * @createTime 2020-11-17 19:48:00
+ */
 public class 搜索旋转排序数组 {
     public int search(int[] nums, int target) {
-        int left=0;
-        int right=nums.length-1;
+        int left = 0,right = nums.length-1;
         while (left<=right){
             int mid = left+(right-left)/2;
             if (nums[mid]==target){
                 return mid;
-            } else if (nums[mid]>target){
-                right = mid-1;
             } else {
-                left = mid+1;
+                if (nums[mid]>=nums[left]){
+                        //左半部分有序
+                    if (nums[mid]>target && nums[left]<=target){
+                        //target在左半部分
+                        right = mid-1;
+                    } else {
+                        left = mid+1;
+                    }
+                } else {
+                    //右半部分有序
+                    if (nums[mid]<target && nums[right]>=target){
+                        //target在右半部分
+                        left = mid+1;
+                    } else {
+                        right = mid-1;
+                    }
+                }
             }
         }
         return -1;
     }
 
+    public static void main(String[] args) {
+        System.out.println(new 搜索旋转排序数组().search(new int[]{3,1},1));
+    }
 }
