f124d5b1c0ef8870d52ce46c3749cc02610f7ea4
ym <2124867925@qq.com>
2020-11-20 16:16:00 +0800

    1120

--- src/greedy/非递增顺序的最小子序列.java
+++ src/greedy/非递增顺序的最小子序列.java
@@ -1,4 +1,26 @@
 package greedy;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class 非递增顺序的最小子序列 {
+    public List<Integer> minSubsequence(int[] nums) {
+        Arrays.sort(nums);
+        int sum = 0;
+        for (int i : nums){
+            sum += i;
+        }
+        List<Integer> res = new ArrayList<>();
+        int resSum = 0;
+        for (int i = nums.length-1;i>=0;i--){
+            res.add(nums[i]);
+            sum-=nums[i];
+            resSum+= nums[i];
+            if (resSum>sum){
+                return res;
+            }
+        }
+        return res;
+    }
 }
