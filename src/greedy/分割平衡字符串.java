0bf3441edee7060bda18c7710206802c3396bacf
ym <2124867925@qq.com>
2020-11-22 20:26:41 +0800

    1122

--- src/greedy/分割平衡字符串.java
+++ src/greedy/分割平衡字符串.java
@@ -1,4 +1,27 @@
 package greedy;
 
 public class 分割平衡字符串 {
+    public int balancedStringSplit(String s) {
+        char[] chars = s.toCharArray();
+        int count=0,cl=0,cr=0;
+        for (char c : chars){
+            if (c=='L'){
+                if (cr==0){
+                    cl++;
+                } else {
+                    cr--;
+                }
+            } else {
+                if (cl==0){
+                    cr++;
+                } else {
+                    cl--;
+                }
+            }
+            if (cl==0 && cr==0){
+                count++;
+            }
+        }
+        return count;
+    }
 }
--- src/greedy/玩筹码.java
+++ src/greedy/玩筹码.java
@@ -1,4 +1,15 @@
 package greedy;
 
 public class 玩筹码 {
+    public int minCostToMoveChips(int[] position) {
+        int odd_count=0,evenCount=0;
+        for (int i : position){
+            if (i%2==0){
+                evenCount++;
+            } else {
+                odd_count++;
+            }
+        }
+        return Math.min(odd_count,evenCount);
+    }
 }
