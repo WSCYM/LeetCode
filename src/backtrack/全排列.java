7e6ffda0b3af93e43578e469d7f2309e9d530770
ym <2124867925@qq.com>
2020-12-09 16:14:49 +0800

    1209

--- src/backtrack/全排列.java
+++ src/backtrack/全排列.java
@@ -1,4 +1,29 @@
 package backtrack;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 public class 全排列 {
+    List<List<Integer>> res = new ArrayList<>();
+    public List<List<Integer>> permute(int[] nums) {
+        LinkedList<Integer> track = new LinkedList<>();
+        backtrack(nums,track);
+        return res;
+    }
+
+    void backtrack(int[] nums, LinkedList<Integer> track){
+        if (nums.length==track.size()){
+            res.add(new ArrayList<>(track));
+            return;
+        }
+        for (int i : nums){
+            if (track.contains(i)){
+                continue;
+            }
+            track.add(i);
+            backtrack(nums,track);
+            track.removeLast();
+        }
+    }
 }
