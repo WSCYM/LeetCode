4ebcddaf09ed61b070a1aa461007eefada6f3cc6
ym <2124867925@qq.com>
2020-12-13 19:10:32 +0800

    1213

--- src/backtrack/活字印刷.java
+++ src/backtrack/活字印刷.java
@@ -1,4 +1,31 @@
 package backtrack;
 
+import java.util.HashSet;
+
 public class 活字印刷 {
+    HashSet<String> hashSet = new HashSet<>();
+    public int numTilePossibilities(String tiles) {
+        StringBuffer sb = new StringBuffer();
+        backtrack(sb,tiles.toCharArray(),new boolean[7]);
+        return hashSet.size();
+    }
+
+    void backtrack(StringBuffer sb,char[] chars,boolean[] visit){
+        if (sb.length()!=0)
+        hashSet.add(sb.toString());
+
+        for (int i = 0;i<chars.length;i++){
+            if (!visit[i]){
+                sb.append(chars[i]);
+                visit[i] = true;
+                backtrack(sb,chars,visit);
+                visit[i] = false;
+                sb.deleteCharAt(sb.length()-1);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        new 活字印刷().numTilePossibilities("A");
+    }
 }
--- src/backtrack/组合总和III.java
+++ src/backtrack/组合总和III.java
@@ -1,4 +1,32 @@
 package backtrack;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 public class 组合总和III {
+    List<List<Integer>> res = new ArrayList<>();
+    public List<List<Integer>> combinationSum3(int k, int n) {
+        backtrack(1,new LinkedList<>(),k,n,0);
+        return res;
+    }
+
+    void backtrack(int begin,LinkedList<Integer> track,int k,int n,int sum){
+        if (sum>n){
+            return;
+        }
+        if (track.size()==k){
+            if (sum==n) res.add(new LinkedList<>(track));
+            return;
+        }
+
+        for (int j = begin;j<=9;j++){
+            if (track.contains(j)){
+                continue;
+            }
+            track.add(j);
+            backtrack(j+1,track,k,n,sum+j);
+            track.removeLast();
+        }
+    }
 }
