9013bed0b3bd12f564e16288a994d47ef7417d57
ym <2124867925@qq.com>
2020-12-27 18:21:38 +0800

    1227

--- src/sort/数组中的第K个最大元素.java
+++ src/sort/数组中的第K个最大元素.java
@@ -1,4 +1,19 @@
 package sort;
 
 public class 数组中的第K个最大元素 {
+    public int findKthLargest(int[] nums, int k) {
+        int idx;
+        for (int i = 0;i<k;i++){
+            idx = i;
+            for (int j=i+1;j<nums.length;j++){
+                if (nums[j]>nums[idx]){
+                    idx = j;
+                }
+            }
+            int tmp = nums[i];
+            nums[i] = nums[idx];
+            nums[idx] = tmp;
+        }
+        return nums[k-1];
+    }
 }
