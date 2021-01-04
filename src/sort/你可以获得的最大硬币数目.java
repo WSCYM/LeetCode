746a2708a92a3cbd214bee420e7abb7a99c8b7cc
ym <2124867925@qq.com>
2021-01-04 20:30:34 +0800

    0104

--- src/sort/你可以获得的最大硬币数目.java
+++ src/sort/你可以获得的最大硬币数目.java
@@ -1,4 +1,36 @@
 package sort;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 public class 你可以获得的最大硬币数目 {
+//    public int maxCoins(int[] piles) {
+//        Integer[] arr = Arrays.stream(piles).boxed().toArray(Integer[]::new);
+//        Arrays.sort(arr,(o1,o2)->o2-o1);
+//        int idx = 1;
+//        int res = 0;
+//        while (idx*3<=arr.length){
+//            res += arr[(idx-1)*2+1];
+//            idx++;
+//        }
+//        return res;
+//    }
+
+    public int maxCoins(int[] piles) {
+        Arrays.sort(piles);
+        int l=0;
+        int r = piles.length-1;
+        int res = 0;
+        while (l<=r){
+            l++;
+            r--;
+            res += piles[r];
+            r--;
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new 你可以获得的最大硬币数目().maxCoins(new int[]{9,8,7,6,5,1,2,3,4}));
+    }
 }
--- src/sort/对链表进行插入排序.java
+++ src/sort/对链表进行插入排序.java
@@ -1,4 +1,20 @@
 package sort;
 
+import classes.ListNode;
+
 public class 对链表进行插入排序 {
+    public ListNode insertionSortList(ListNode head) {
+        ListNode res = new ListNode(Integer.MIN_VALUE);
+        while (head!=null){
+            ListNode p = res;
+            while (p.next!=null && p.next.val<head.val){
+                p=p.next;
+            }
+            ListNode node = new ListNode(head.val);
+            node.next = p.next;
+            p.next = node;
+            head = head.next;
+        }
+        return res.next;
+    }
 }
