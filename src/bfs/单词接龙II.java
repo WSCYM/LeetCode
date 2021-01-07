0a26ae7d011303b10fd7bfb72a2ed8f12971ad26
ym <2124867925@qq.com>
2021-01-07 20:02:35 +0800

    0107

--- src/bfs/单词接龙II.java
+++ src/bfs/单词接龙II.java
@@ -4,59 +4,61 @@ import java.util.*;
 
 public class 单词接龙II {
 
-
+    //dfs+bfs超时
     int size;
     List<List<String>> res = new ArrayList<>();
     public List<List<String>> findLadders(String beginWord, String endWord, List<String> wordList) {
+        if (!wordList.contains(endWord)) return res;
         boolean[] visited = new boolean[wordList.size()];
         List<String> path = new ArrayList<>();
         path.add(beginWord);
         if (wordList.contains(beginWord)) visited[wordList.indexOf(beginWord)]=true;
-        HashMap<String, List<String>> hashMap = bfs(visited, beginWord, endWord, wordList);
+        HashMap<String, Set<String>> hashMap = bfs(visited, beginWord, endWord, wordList);
         backtracking(path,hashMap,endWord);
 
         return res;
     }
 
-    void backtracking(List<String> path,HashMap<String,List<String>> hashMap,String endWord){
+    void backtracking(List<String> path,HashMap<String,Set<String>> hashMap,String endWord){
         if (path.get(path.size()-1).equals(endWord) && path.size()==size){
             res.add(new ArrayList<>(path));
         }
         if (path.size()>=size) return;
 
-        List<String> list = hashMap.getOrDefault(path.get(path.size() - 1),new ArrayList<>());
-        for (String s : list){
+        Set<String> set = hashMap.getOrDefault(path.get(path.size() - 1),new HashSet<>());
+        for (String s : set){
                 path.add(s);
                 backtracking(path,hashMap,endWord);
                 path.remove(path.get(path.size()-1));
         }
     }
 
-    HashMap<String,List<String>> bfs(boolean[] visited,String beginWord,String endWord,List<String> wordList){
-        HashMap<String,List<String>> hashMap = new HashMap<>();
+    HashMap<String,Set<String>> bfs(boolean[] visited,String beginWord,String endWord,List<String> wordList){
+        HashMap<String,Set<String>> hashMap = new HashMap<>();
         Queue<String> queue = new LinkedList<>();
         queue.add(beginWord);
-        size = 0;
+        size = 1;
         boolean findEndWord = false;
         while (!queue.isEmpty()){
+            List<Integer> visitedInThisTimes =new ArrayList<>();
+            int curSize = queue.size();
             size++;
-            String remove = queue.remove();
-            List<Integer> visitedInthisTimes =new ArrayList<>();
-            for (int i = 0;i<wordList.size();i++){
-                if (!visited[i] && canToword2(remove,wordList.get(i))){
-                    List<String> list = hashMap.getOrDefault(remove, new ArrayList<>());
-                    if (wordList.get(i).equals(endWord)) {
-                        findEndWord = true;
+            for (int t =0;t<curSize;t++){
+                String remove = queue.remove();
+                for (int i = 0;i<wordList.size();i++){
+                    if (!visited[i] && canToword2(remove,wordList.get(i))){
+                        Set<String> set = hashMap.getOrDefault(remove, new HashSet<>());
+                        if (wordList.get(i).equals(endWord)) {
+                            findEndWord = true;
+                        }
+                        queue.add(wordList.get(i));
+                        visitedInThisTimes.add(i);
+                        set.add(wordList.get(i));
+                        hashMap.put(remove,set);
                     }
-                    queue.add(wordList.get(i));
-
-                    visitedInthisTimes.add(i);
-
-                    list.add(wordList.get(i));
-                    hashMap.put(remove,list);
                 }
             }
-            for (int i : visitedInthisTimes){
+            for (int i : visitedInThisTimes){
                 visited[i] = true;
             }
             if (findEndWord) break;
@@ -81,12 +83,14 @@ public class 单词接龙II {
 
     public static void main(String[] args) {
         List<String> list = new ArrayList<>();
-        list.add("hot");
-        list.add("dot");
-        list.add("dog");
-        list.add("lot");
-        list.add("log");
-        list.add("cog");
-        new 单词接龙II().findLadders("hit","cog",list);
+        list.add("ted");
+        list.add("tex");
+        list.add("red");
+        list.add("tax");
+        list.add("tad");
+        list.add("den");
+        list.add("rex");
+        list.add("pee");
+        new 单词接龙II().findLadders("red","tax",list);
     }
 }
