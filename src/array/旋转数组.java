876ed931ef3c953465f466c325dd6eaeff1828c0
ym <2124867925@qq.com>
2021-01-08 13:36:23 +0800

    0108

--- src/array/旋转数组.java
+++ src/array/旋转数组.java
@@ -1,4 +1,21 @@
 package array;
 
 public class 旋转数组 {
+    public void rotate(int[] nums, int k) {
+        k = k%nums.length;
+        revArr(nums,nums.length-k,nums.length-1);
+        revArr(nums,0,nums.length-1-k);
+        revArr(nums,0,nums.length-1);
+    }
+
+    void revArr(int[] nums,int start,int end){
+        int tmp;
+        while (start<end){
+            tmp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = tmp;
+            start++;
+            end--;
+        }
+    }
 }
