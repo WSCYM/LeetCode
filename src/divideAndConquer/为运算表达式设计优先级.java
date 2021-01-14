023deadd49f02537f0f59e32d654f2601a0ce0f5
ym <2124867925@qq.com>
2021-01-14 23:03:45 +0800

    0114

--- src/divideAndConquer/为运算表达式设计优先级.java
+++ src/divideAndConquer/为运算表达式设计优先级.java
@@ -1,4 +1,34 @@
 package divideAndConquer;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class 为运算表达式设计优先级 {
+    public List<Integer> diffWaysToCompute(String input) {
+        List<Integer> res = new ArrayList<>();
+        char[] chars = input.toCharArray();
+        for (int i = 0;i<chars.length;i++){
+            if (chars[i]=='*'||chars[i]=='+'||chars[i]=='-'){
+                List<Integer> left = diffWaysToCompute(input.substring(0, i));
+                List<Integer> right = diffWaysToCompute(input.substring(i + 1));
+                for (int l : left){
+                    for (int r : right){
+                        if (chars[i] =='*'){
+                            res.add(l*r);
+                        }
+                        if (chars[i] =='+'){
+                            res.add(l+r);
+                        }
+                        if (chars[i] =='-'){
+                            res.add(l-r);
+                        }
+                    }
+                }
+            }
+        }
+        if (res.size() == 0) {
+            res.add(Integer.valueOf(input));
+        }
+        return res;
+    }
 }
--- src/divideAndConquer/漂亮数组.java
+++ src/divideAndConquer/漂亮数组.java
@@ -1,2 +1,19 @@
-package divideAndConquer;public class 漂亮数组 {
+package divideAndConquer;
+
+public class 漂亮数组 {
+    public int[] beautifulArray(int N) {
+        if (N == 1) {
+            return new int[]{1};
+        }
+        int[] odd = beautifulArray((N + 1) / 2);
+        int[] even = beautifulArray(N / 2);
+        int[] res = new int[N];
+        for (int i = 0; i < odd.length; i++) {
+            res[i] = odd[i] * 2 - 1;
+        }
+        for (int i = 0; i < even.length; i++) {
+            res[i + odd.length] = even[i] * 2;
+        }
+        return res;
+    }
 }
