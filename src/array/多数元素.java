97e3c67ea55635b4d24aa45b4f22c71ead1b6386
ym <2124867925@qq.com>
2021-01-19 18:04:42 +0800

    0119

--- src/array/多数元素.java
+++ src/array/多数元素.java
@@ -1,4 +1,48 @@
 package array;
 
-public class 多数元素 {
+//摩尔投票
+//public class 多数元素 {
+//    public int majorityElement(int[] nums) {
+//        int val = nums[0];
+//        int sum = 0;
+//        for (int i : nums){
+//            if (i==val){
+//                sum++;
+//            } else {
+//                if (sum==0){
+//                    sum=1;
+//                    val = i;
+//                } else {
+//                    sum--;
+//                }
+//            }
+//        }
+//        return val;
+//    }
+//}
+
+class 多数元素 {
+
+    private int helper(int[] nums, int left, int right) {
+        if (left>=right) return nums[left];
+        int mid = left + (right-left)/2;
+        int numL = helper(nums,left,mid);
+        int numR = helper(nums,mid+1,right);
+        if (numL==numR) return numL;
+        int l = 0;
+        int r = 0;
+        for (int i = left;i<=right;i++){
+            if (nums[i]==numL) l++;
+            if (nums[i]==numR) r++;
+        }
+        return l>r?numL:numR;
+    }
+
+    public int majorityElement(int[] nums) {
+        return helper(nums,0,nums.length-1);
+    }
+
+    public static void main(String[] args) {
+        new 多数元素().majorityElement(new int[]{6,5,5});
+    }
 }
--- src/divideAndConquer/连续数列.java
+++ src/divideAndConquer/连续数列.java
@@ -1,4 +1,38 @@
 package divideAndConquer;
 
 public class 连续数列 {
+    public int maxSubArray(int[] nums) {
+        return helper(nums,0,nums.length-1);
+    }
+
+    private int helper(int[] nums,int lo,int hi){
+        if (lo>=hi){
+            return nums[lo];
+        }
+        int mid = lo + (hi-lo)/2;
+        int left = helper(nums,lo,mid-1);
+        int right = helper(nums,mid+1,hi);
+        int maxSumMidL = 0;
+        int maxSumMidR = 0;
+        int sumMidL = 0;
+        int sumMidR = 0;
+        for (int i = mid-1;i>=0;i--){
+            sumMidL+=nums[i];
+            if (sumMidL>maxSumMidL){
+                maxSumMidL = sumMidL;
+            }
+        }
+        for (int i = mid+1;i<=hi;i++){
+            sumMidR+=nums[i];
+            if (sumMidR>maxSumMidR){
+                maxSumMidR = sumMidR;
+            }
+        }
+        int maxSumMid = maxSumMidL+maxSumMidR+nums[mid];
+        return Math.max(Math.max(left,right),maxSumMid);
+    }
+
+    public static void main(String[] args) {
+        new 连续数列().maxSubArray(new int[]{8,-19,5,-4,20});
+    }
 }
