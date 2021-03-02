b8ffc2468f11d11e3a781d187c6864eda2ae42b9
ym <2124867925@qq.com>
2021-03-02 20:42:49 +0800

    0302

--- src/math/七进制数.java
+++ src/math/七进制数.java
@@ -1,4 +1,17 @@
 package math;
 
 public class 七进制数 {
+    public String convertToBase7(int num) {
+        if (num==0) return "0";
+        StringBuffer res = new StringBuffer();
+        boolean lowThanZero = num<0?true:false;
+        num = Math.abs(num);
+        while (num>0){
+            int i = num % 7;
+            res.insert(0,i);
+            num /=7;
+        }
+        if (lowThanZero) res.insert(0,"-");
+        return res.toString();
+    }
 }
