d71490d684838358cb43593694d1d41963be0c3e
ym <2124867925@qq.com>
2021-03-23 19:55:40 +0800

    0323

--- test/Student.java
+++ test/Student.java
@@ -1,4 +1,36 @@
-package PACKAGE_NAME;
+public class Student implements Cloneable{
+    private int age;
+    private String name;
+    private Subject subject;
 
-public class Student {
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Subject getSubject() {
+        return subject;
+    }
+
+    public void setSubject(Subject subject) {
+        this.subject = subject;
+    }
+
+    @Override
+    public Object clone() throws CloneNotSupportedException {
+        Student clone = (Student) super.clone();
+        clone.setSubject((Subject) subject.clone());
+        return clone;
+    }
 }
--- test/Subject.java
+++ test/Subject.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
+public class Subject implements Cloneable{
+    private int soc;
 
-public class Subject {
+    public int getSoc() {
+        return soc;
+    }
+
+    public void setSoc(int soc) {
+        this.soc = soc;
+    }
+
+    @Override
+    public Object clone() throws CloneNotSupportedException {
+        return  super.clone();
+    }
 }
