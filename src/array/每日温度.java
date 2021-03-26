de5bb4d21bcf83f2f849fd68267ef8ab619bf492
ym <2124867925@qq.com>
2021-03-26 11:18:26 +0800

    0326

--- src/array/每日温度.java
+++ src/array/每日温度.java
@@ -1,2 +1,25 @@
-package array;public class 每日温度 {
+package array;
+
+public class 每日温度 {
+    public int[] dailyTemperatures(int[] T) {
+        int length = T.length;
+        int[] result = new int[length];
+
+        for (int i = length - 2; i >= 0; i--) {
+            for (int j = i + 1; j < length; j+= result[j]) {
+                if (T[j] > T[i]) {
+                    result[i] = j - i;
+                    break;
+                } else if (result[j] == 0) {
+                    result[i] = 0;
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        new 每日温度().dailyTemperatures(new int[]{73,74,75,71,69,72,76,73});
+    }
 }
