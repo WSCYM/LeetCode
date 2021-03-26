d4d33824fa812ab42eb2e9708bc582fd13060e6a
ym <2124867925@qq.com>
2021-03-26 16:48:17 +0800

    0326

--- src/JZOffer/复杂链表的复制.java
+++ src/JZOffer/复杂链表的复制.java
@@ -1,4 +1,24 @@
 package JZOffer;
 
+import classes.Node;
+
+import java.util.HashMap;
+
 public class 复杂链表的复制 {
+    public Node copyRandomList(Node head) {
+        HashMap<Node,Node> hashMap = new HashMap<>();
+        Node cur = head;
+        while (cur!=null){
+            Node node = new Node(cur.val);
+            hashMap.put(cur,node);
+            cur = cur.next;
+        }
+        cur = head;
+        while (cur!=null){
+            hashMap.get(cur).next = hashMap.get(cur.next);
+            hashMap.get(cur).random = hashMap.get(cur.random);
+            cur = cur.next;
+        }
+        return hashMap.get(head);
+    }
 }
--- src/stackAndQueue/每日温度.java
+++ src/stackAndQueue/每日温度.java
@@ -1,4 +1,24 @@
 package stackAndQueue;
 
+import java.util.Stack;
+
 public class 每日温度 {
+    public int[] dailyTemperatures(int[] T) {
+        Stack<Integer> stack = new Stack<>();
+        int[] ret = new int[T.length];
+        for (int i = 0;i<T.length;i++){
+            if (!stack.isEmpty()&&T[stack.peek()]<T[i]){
+                while (!stack.isEmpty() && T[stack.peek()]<T[i]){
+                    Integer pop = stack.pop();
+                    ret[pop] = i-pop;
+                }
+            }
+            stack.push(i);
+        }
+        return ret;
+    }
+
+    public static void main(String[] args) {
+        new 每日温度().dailyTemperatures(new int[]{73,74,75,71,69,72,76,73});
+    }
 }
