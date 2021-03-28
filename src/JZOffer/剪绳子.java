10b85087db20a085859f19acee8b762e76bf4e5e
ym <2124867925@qq.com>
2021-03-28 11:54:41 +0800

    0328

--- src/JZOffer/剪绳子.java
+++ src/JZOffer/剪绳子.java
@@ -1,4 +1,11 @@
 package JZOffer;
 
 public class 剪绳子 {
+    public int cuttingRope(int n) {
+        if (n<=3) return n-1;
+        if ((n % 3)==0) return (int) Math.pow(3,n/3);
+        if ((n % 3)==1) return (int) Math.pow(3,n/3-1)*4;
+        if ((n % 3)==2) return (int) Math.pow(3,n/3)*2;
+        return -1;
+    }
 }
