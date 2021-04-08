defb48b6b9e1bd409736e363c8fea3f998c2fa84
ym <2124867925@qq.com>
2021-04-08 21:47:48 +0800

    0408

--- src/JZOffer/把字符串转换成整数.java
+++ src/JZOffer/把字符串转换成整数.java
@@ -1,4 +1,38 @@
 package JZOffer;
 
 public class 把字符串转换成整数 {
+    public int strToInt(String str) {
+        if (str.length()==0) return 0;
+        char[] chars = str.toCharArray();
+        int i = 0;
+        for (;i<chars.length;i++){
+            if(chars[i]!=' ') break;
+        }
+        boolean lessThan0 = false;
+        if (i>=chars.length || (chars[i]!='+' && chars[i]!='-' && !(chars[i]>='0' && chars[i] <='9'))){
+            return 0;
+        }
+        if (chars[i]=='+'){
+            i++;
+        } else if (chars[i]=='-') {
+            i++;
+            lessThan0 = true;
+        }
+        long res = 0;
+        while (i<chars.length && chars[i]>='0' && chars[i] <='9'){
+            res = res*10+(chars[i]-'0');
+            if (!lessThan0 && res>Integer.MAX_VALUE){
+                return Integer.MAX_VALUE;
+            }
+            if (lessThan0 && -res<Integer.MIN_VALUE){
+                return Integer.MIN_VALUE;
+            }
+            i++;
+        }
+        return (int) (lessThan0?-res:res);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new 把字符串转换成整数().strToInt("9223372036854775808"));
+    }
 }
--- src/JZOffer/队列的最大值.java
+++ src/JZOffer/队列的最大值.java
@@ -1,4 +1,38 @@
 package JZOffer;
 
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class 队列的最大值 {
+}
+
+class MaxQueue {
+    Queue<Integer> queue;
+    Deque<Integer> deque;
+    public MaxQueue() {
+        queue= new LinkedList();
+        deque= new LinkedList();
+    }
+
+    public int max_value() {
+        return deque.isEmpty()?-1:deque.getFirst();
+    }
+
+    public void push_back(int value) {
+        queue.add(value);
+        while (!deque.isEmpty() && deque.getLast()<value){
+            deque.removeLast();
+        }
+        deque.addLast(value);
+    }
+
+    public int pop_front() {
+        if (queue.isEmpty()) return -1;
+        Integer remove = queue.remove();
+        if (remove.equals(deque.getFirst())){
+            deque.removeFirst();
+        }
+        return remove;
+    }
 }
