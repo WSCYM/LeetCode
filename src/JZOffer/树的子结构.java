7d56ecab1f3c0367224563bf99b4b8107be14f60
ym <2124867925@qq.com>
2021-04-12 20:18:29 +0800

    0412

--- src/JZOffer/树的子结构.java
+++ src/JZOffer/树的子结构.java
@@ -1,4 +1,16 @@
 package JZOffer;
 
+import classes.TreeNode;
+
 public class 树的子结构 {
+    public boolean isSubStructure(TreeNode A, TreeNode B) {
+        if (A==null || B==null) return false;
+        return recur(A,B) || isSubStructure(A.left,B) || isSubStructure(A.right,B);
+    }
+
+    boolean recur(TreeNode A,TreeNode B){
+        if (B == null) return true;
+        if (A==null || A.val!=B.val) return false;
+        return recur(A.left,B.left) && recur(A.right,B.right);
+    }
 }
