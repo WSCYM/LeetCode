7b1b540a3cf8028387fb9943518cc0028df8ecd3
ym <17854284792@163.com>
2021-05-19 17:46:58 +0800

    0519

--- src/leetcode/editor/cn/top100-[2]两数相加.java
+++ src/leetcode/editor/cn/top100-[2]两数相加.java
@@ -1,5 +1,6 @@
 package leetcode.editor.cn;
 import classes.ListNode;
+import classes.Node;
 //给你两个 非空 的链表，表示两个非负的整数。它们每位数字都是按照 逆序 的方式存储的，并且每个节点只能存储 一位 数字。 
 //
 // 请你将两个数相加，并以相同形式返回一个表示和的链表。 
@@ -57,35 +58,51 @@ import classes.ListNode;
  * }
  */
 class Solution {
+//    //迭代
+//    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+//        int add = 0;
+//        ListNode head = new ListNode(0);
+//        ListNode p = head;
+//        while (l1!=null && l2!=null){
+//            int val = l1.val+l2.val+add;
+//            p.next = new ListNode(val%10);
+//            p=p.next;
+//            if (val>=10){
+//                add = 1;
+//            } else {
+//                add = 0;
+//            }
+//            l1=l1.next;
+//            l2=l2.next;
+//        }
+//        ListNode tail = l1==null?l2:l1;
+//        while (tail!=null){
+//            int val = tail.val+add;
+//            p.next = new ListNode(val%10);
+//            p=p.next;
+//            if (val>=10){
+//                add = 1;
+//            } else {
+//                add = 0;
+//            }
+//            tail=tail.next;
+//        }
+//        if (add!=0){
+//            p.next = new ListNode(1);
+//        }
+//        return head.next;
+//    }
+    //递归
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        int add = 0;
-        ListNode head = new ListNode(0);
-        ListNode p = head;
-        while (l1!=null && l2!=null){
-            int val = l1.val+l2.val+add;
-            p.next = new ListNode(val%10);
-            p=p.next;
-            if (val>=10){
-                add = 1;
-            } else {
-                add = 0;
-            }
-            l1=l1.next;
-            l2=l2.next;
-        }
-        ListNode tail = l1==null?l2:l1;
-        while (tail!=null){
-            int val = tail.val+add;
-            p.next = new ListNode(val%10);
-            p=p.next;
-            if (val>=10){
-                add = 1;
-            } else {
-                add = 0;
-            }
-            tail=tail.next;
-        }
-        return head.next;
+        return r(l1,l2,0);
+    }
+
+    private ListNode r(ListNode l1,ListNode l2,int add){
+        if (l1==null && l2==null && add==0) return null;
+        int val = add+(l1!=null?l1.val:0)+(l2!=null?l2.val:0);
+        ListNode curNode = new ListNode(val%10);
+        curNode.next = r(l1==null?null:l1.next,l2==null?null:l2.next,val>=10?1:0);
+        return curNode;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
